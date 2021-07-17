4cdb5c58ae12426e1a7c10149fab905feedfebea
FieryInferno <bagassetia271@gmail.com>
2021-07-17 11:10:00 +0700

    Upload data warga

--- application/controllers/Admin/Warga.php
+++ application/controllers/Admin/Warga.php
@@ -50,7 +50,6 @@ class Warga extends CI_Controller {
     $config['upload_path']    = './assets/'; //path upload
     $config['file_name']      = $fileName;  // nama file
     $config['allowed_types']  = 'xls|xlsx|csv'; //tipe file yang diperbolehkan
-    $config['max_size']       = 10000; // maksimal sizze
 
     $this->load->library('upload'); //meload librari upload
     $this->upload->initialize($config);
@@ -75,19 +74,22 @@ class Warga extends CI_Controller {
 
     for ($row = 2; $row <= $highestRow; $row++) {                  //  Read a row of data into an array                 
       $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);   
-
-      // Sesuaikan key array dengan nama kolom di database                                                         
+      // Sesuaikan key array dengan nama kolom di database   
+      $status_perkawinan  = strtolower($rowData[0][5]) == 'menikah' ? 'menikah' : 'belum_menikah';                                                      
       $data = array(
         "nik"               => $rowData[0][0],
         "nama"              => $rowData[0][1],
         "tempat_lahir"      => $rowData[0][2],
         "tanggal_lahir"     => $rowData[0][3],
         "alamat"            => $rowData[0][4],
-        "status_perkawinan" => $rowData[0][5],
+        "status_perkawinan" => $status_perkawinan
       );
 
       $insert = $this->db->insert("warga",$data);
     }
+
+    file_exists('./assets/' . $this->upload->data('file_name')) ? unlink('./assets/' . $this->upload->data('file_name')) : '';
+
     $this->session->set_flashdata('pesan', '
       <div class="alert alert-success" role="alert">
         Berhasil Import Data
